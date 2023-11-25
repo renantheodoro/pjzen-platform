7a49239a0a90bfc8978c1796bc2885703af2f467
renan.theodoro <renan.b.theodoro@gmail.com>
2023-11-25 11:21:53 -0300

    feat: add alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="showAlert" class="alert alert--orange">
+    <div class="alert__icon">
+      <img src="@/assets/images/icons/file-exclamation.svg" />
+    </div>
+
+    <div>
+      <span class="alert__title">{{ title }}</span>
+      <span class="alert__text"><slot></slot> </span>
+    </div>
+
+    <a @click.prevent="close" class="alert__close">
+      <img src="@/assets/images/icons/times.svg" />
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "app-alert",
+
+  props: {
+    title: String,
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      showAlert: this.show ?? false,
+    };
+  },
+
+  methods: {
+    close() {
+      this.showAlert = false;
+    },
+  },
+};
+</script>
+<style lang=""></style>
