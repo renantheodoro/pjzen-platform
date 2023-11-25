ca14bc8cc48c16a4348807f00228a4209261d43b
renan.theodoro <renan.b.theodoro@gmail.com>
2023-11-25 11:21:34 -0300

    feat: add project base

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Sidebar />
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from '@/components/Sidebar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from "vue-router";
+import routes from "./routes";
 
-createApp(App).mount('#app')
+// CSS import
+import "./assets/styles/index.scss";
+
+// Route definition
+const router = new VueRouter.createRouter({
+    linkActiveClass: "active",
+    history: VueRouter.createWebHistory(),
+    routes,
+    scrollBehavior() {
+        // always scroll to top
+        return { top: 0 };
+    },
+});
+
+// App config
+const app = createApp(App);
+
+// App Usages
+app.use(router);
+
+// App mount
+app.mount("#app");
